0995f827733b52a918e6ee64820345a74f57b4e0
Seungmin Jeong <seungmin1266@gmail.com>
2021-12-15 16:44:02 +0900

    Update Home.js : Add some basic code

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
 import React, {Component} from 'react';
-import { BrowserRouter, Route, Link, Switch} from 'react-router-dom';
+import {BrowserRouter as Route,Router, Routes, withRouter} from "react-router-dom";
+
+
 
 import Home from "./route/Home";
 import Intro from "./route/Intro";
 
 function App() {
-  return (
-    <Router>
-            <Routes>
-                <RouteLayout
-                    exact
-                    path = "/"
-                    layout = {DefaultLayout}
-                    component = {withRouter(Home)} />
-                    
-                <RouteLayout
-                    exact
-                    path = "/Intro"
-                    layout = {DefaultLayout}
-                    component = {withRouter(Intro)} />
-            </Routes>
-        </Router>
-  );
+  return(
+    <div>
+        hi
+    </div>
+)
 }
 
 export default App;

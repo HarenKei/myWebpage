c66da792f993db50267e12f4b3eae1357f4df966
Seungmin Jeong <seungmin1266@gmail.com>
2021-12-15 15:52:58 +0900

    Update App.js : istall react-router-dom and import.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import { BrowserRouter, Route, Link, Switch} from 'react-router-dom';
 
 function App() {
   return (

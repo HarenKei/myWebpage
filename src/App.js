6ca5a1e6793804e020ad12ad3a3ea94c72260a4a
Seungmin Jeong <seungmin1266@gmail.com>
2021-12-15 16:15:21 +0900

    Update App.js : import another page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React, {Component} from 'react';
 import { BrowserRouter, Route, Link, Switch} from 'react-router-dom';
 
+import Home from "./route/Home";
+import Intro from "./route/Intro";
+
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+            <Routes>
+                <RouteLayout
+                    exact
+                    path = "/"
+                    layout = {DefaultLayout}
+                    component = {withRouter(Home)} />
+                    
+                <RouteLayout
+                    exact
+                    path = "/Intro"
+                    layout = {DefaultLayout}
+                    component = {withRouter(Intro)} />
+            </Routes>
+        </Router>
   );
 }
 

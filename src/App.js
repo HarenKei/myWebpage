2e0a967e9528457a78001aea312cb41c0390f1de
Seungmin Jeong <seungmin1266@gmail.com>
2021-12-16 19:30:57 +0900

    Add react-color

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
-import {Route, Routes} from "react-router-dom";
+import {Route, Routes, Link} from "react-router-dom";
 
 
 
 import Home from "./route/Home";
-import Intro from "./route/Intro";
+import Intro from './route/Intro/Intro';
+import Thumbnail from './route/Thumbnail/Thumbnail';
 
 function App() {
   return(
     <div>
       <Routes>
         <Route path="/" element={<Home/>} />
-        <Route path="/Intro" component={<Intro/>} />
+        <Route path="/Intro" element={<Intro/>} />
+        <Route path="/Thumbnail" element={<Thumbnail/>}/>
+        
       </Routes>
-      
+      <Link to="/Thumbnail"> Thumbnail Maker </Link>
+      <Link to="/Intro">Introduce my self</Link>
     </div>
 )
 }
--- src/route/Home/Home.js
+++ src/route/Home/Home.js
@@ -2,15 +2,19 @@ import React from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faFacebook, faInstagram } from "@fortawesome/free-brands-svg-icons";
 
+import {Route, Routes, Link} from 'react-router-dom'
+
 import './Home.css';
 
 const Home = () => {
     return(
         <div>
-            <h1>Hello world</h1>
-            <FontAwesomeIcon icon={faGithub} className="github" />
-            <FontAwesomeIcon icon={faFacebook} className="facebook" />
-            <FontAwesomeIcon icon={faInstagram} className="instagram" />
+            <h1>𝙎𝙏𝘼𝙍𝙂𝘼𝙕𝙀𝙍</h1>
+            <h1>HarenKei</h1>
+            <a href="https://github.com/HarenKei"><FontAwesomeIcon icon={faGithub} className="github" /></a>
+            <a href="https://www.facebook.com/HarenKei"><FontAwesomeIcon icon={faFacebook} className="facebook" /></a>
+            <a href="https://www.instagram.com/min_from00/"><FontAwesomeIcon icon={faInstagram} className="instagram" /></a>
+
         </div>
     )
 }
